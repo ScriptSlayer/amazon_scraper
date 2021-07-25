5cc4daf0afc4162b866c2f92bfadae755d55b003
ScriptSlayer <7130745+ScriptSlayer@users.noreply.github.com>
2021-07-25 17:18:50 +0200

    initialise API

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const request = require('request-promise');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const apiKey = '204206f8e571438b67746769c82069f2';
+
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Amazon Scraper API.');
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
